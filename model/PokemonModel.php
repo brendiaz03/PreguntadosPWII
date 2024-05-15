21f3d4cbb5b1aeb46e6f792da946d7f4e5769ab5
Felipe Weigel Muñoz <94728784+FeliWeigel@users.noreply.github.com>
2024-05-14 21:14:26 -0300

    Update PokemonModel.php

--- model/PokemonModel.php
+++ model/PokemonModel.php
@@ -1,9 +1,5 @@
 <?php
     class PokemonModel {
-<<<<<<< HEAD
-
-=======
->>>>>>> a9beca70fcbbf31bb29d6f78988ec00bc3f86d61
         private $database;
 
         public function __construct($database)
@@ -13,16 +9,12 @@
 
         public function getAllPokemon()
         {
-<<<<<<< HEAD
             return $this -> database -> query("SELECT * FROM Pokemon");
-=======
             return $this -> $database -> query("SELECT * FROM Pokemon");
->>>>>>> a9beca70fcbbf31bb29d6f78988ec00bc3f86d61
         }
 
         public function getPokemon($id)
         {
-<<<<<<< HEAD
             return $this -> database -> query("SELECT * FROM Pokemon WHERE id = $id");
         }
 
@@ -32,9 +24,7 @@
 
         public function deletePokemon($id){
             return $this -> database -> query("DELETE FROM pokemon WHERE id = $id");
-=======
             return $this -> $database -> query("SELECT * FROM Pokemon WHERE id = $id");
->>>>>>> a9beca70fcbbf31bb29d6f78988ec00bc3f86d61
         }
 
         public function editPokemon($id, $nombre, $tipo, $descripcion)
