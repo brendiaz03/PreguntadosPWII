d3f7ca0300e5b10884922023a2a1ac8bbc566890
Brenda Diaz <brendiaaz03@gmail.com>
2024-05-26 17:59:25 -0300

    subo otra base

--- controller/PreguntaController.php
+++ controller/PreguntaController.php
@@ -13,90 +13,89 @@ class PreguntaController
 
     public function get()
     {
-        $pokemones = $this->model->getAllPokemon();
         $user = false;
         if($_COOKIE['usernameCookie'] != null){
             $user = $_COOKIE['usernameCookie'];
         }
 
-        $this->presenter->render("view/pokemonListView.mustache", ["pokemones" => $pokemones, "user" => $user]);
+        $this->presenter->render("view/login.mustache", ["user" => $user]);
     }
 
-    public function delete()
-    {
-        $id = $_POST["id"];
-        $this->model->deletePokemon($id);
-        header("location:/pokedex/index.php");
-        exit();
-    }
-
-    public function add()
-    {
-        $pokemonId = $_POST["id"];
-        $pokemonNombre = $_POST["nombre"];
-        $pokemonTipo = $_POST["tipo"];
-        $pokemonDescripcion = $_POST["descripcion"];
-        $imagenTmp = $_FILES['imagen']['tmp_name'];
-        $this->model->addPokemon($pokemonId, $pokemonNombre, $pokemonTipo, $pokemonDescripcion, $imagenTmp);
-        header("location:/pokedex/index.php");
-        exit();
-    }
-
-    public function search()
-    {
-        $busqueda = $_POST["busqueda"];
-        $pokemones = $this->model->searchPokemon($busqueda);
-        $user = false;
-        if($_COOKIE['usernameCookie'] != null){
-            $user = $_COOKIE['usernameCookie'];
-        }
-        $this->presenter->render("view/pokemonListView.mustache", ["pokemones" => $pokemones, "user" => $user]);
-    }
-
-    public function addView()
-    {
-        $user = false;
-        if($_COOKIE['usernameCookie'] != null){
-            $user = $_COOKIE['usernameCookie'];
-        }
-        $this->presenter->render("view/agregarPokemonView.mustache", ["user" => $user]);
-    }
-
-    public function edit()
-    {
-        $pokemonId = $_POST["id"];
-        $pokemonNombre = $_POST["nombre"];
-        $pokemonTipo = $_POST["tipo"];
-        $pokemonDescripcion = $_POST["descripcion"];
-        $imagenTmp = $_FILES['imagen']['tmp_name'];
-
-        $this->model->editPokemon($pokemonId, $pokemonNombre, $pokemonTipo, $pokemonDescripcion, $imagenTmp);
-        header("location:/pokedex/index.php");
-        exit();
-    }
-
-    public function editView()
-    {
-        $id = $_POST["id"];
-        $pokemon = $this->model->getPokemon($id);
-        $user = false;
-        if($_COOKIE['usernameCookie'] != null){
-            $user = $_COOKIE['usernameCookie'];
-        }
-        $this->presenter->render("view/editarPokemonView.mustache", ["pokemon" => $pokemon, "user" => $user]);
-    }
-
-
-    public function detallePokemonView()
-    {
-        $id = $_GET["id"];
-        $pokemon = $this->model->getPokemon($id);
-        $user = false;
-        if($_COOKIE['usernameCookie'] != null){
-            $user = $_COOKIE['usernameCookie'];
-        }
-        $this->presenter->render("view/detallePokemonView.mustache", ["pokemon" => $pokemon, "user" => $user]);
-    }
+//    public function delete()
+//    {
+//        $id = $_POST["id"];
+//        $this->model->deletePokemon($id);
+//        header("location:/pokedex/index.php");
+//        exit();
+//    }
+//
+//    public function add()
+//    {
+//        $pokemonId = $_POST["id"];
+//        $pokemonNombre = $_POST["nombre"];
+//        $pokemonTipo = $_POST["tipo"];
+//        $pokemonDescripcion = $_POST["descripcion"];
+//        $imagenTmp = $_FILES['imagen']['tmp_name'];
+//        $this->model->addPokemon($pokemonId, $pokemonNombre, $pokemonTipo, $pokemonDescripcion, $imagenTmp);
+//        header("location:/pokedex/index.php");
+//        exit();
+//    }
+//
+//    public function search()
+//    {
+//        $busqueda = $_POST["busqueda"];
+//        $pokemones = $this->model->searchPokemon($busqueda);
+//        $user = false;
+//        if($_COOKIE['usernameCookie'] != null){
+//            $user = $_COOKIE['usernameCookie'];
+//        }
+//        $this->presenter->render("view/pokemonListView.mustache", ["pokemones" => $pokemones, "user" => $user]);
+//    }
+//
+//    public function addView()
+//    {
+//        $user = false;
+//        if($_COOKIE['usernameCookie'] != null){
+//            $user = $_COOKIE['usernameCookie'];
+//        }
+//        $this->presenter->render("view/agregarPokemonView.mustache", ["user" => $user]);
+//    }
+//
+//    public function edit()
+//    {
+//        $pokemonId = $_POST["id"];
+//        $pokemonNombre = $_POST["nombre"];
+//        $pokemonTipo = $_POST["tipo"];
+//        $pokemonDescripcion = $_POST["descripcion"];
+//        $imagenTmp = $_FILES['imagen']['tmp_name'];
+//
+//        $this->model->editPokemon($pokemonId, $pokemonNombre, $pokemonTipo, $pokemonDescripcion, $imagenTmp);
+//        header("location:/pokedex/index.php");
+//        exit();
+//    }
+//
+//    public function editView()
+//    {
+//        $id = $_POST["id"];
+//        $pokemon = $this->model->getPokemon($id);
+//        $user = false;
+//        if($_COOKIE['usernameCookie'] != null){
+//            $user = $_COOKIE['usernameCookie'];
+//        }
+//        $this->presenter->render("view/editarPokemonView.mustache", ["pokemon" => $pokemon, "user" => $user]);
+//    }
+//
+//
+//    public function detallePokemonView()
+//    {
+//        $id = $_GET["id"];
+//        $pokemon = $this->model->getPokemon($id);
+//        $user = false;
+//        if($_COOKIE['usernameCookie'] != null){
+//            $user = $_COOKIE['usernameCookie'];
+//        }
+//        $this->presenter->render("view/detallePokemonView.mustache", ["pokemon" => $pokemon, "user" => $user]);
+//    }
 
 }
 
