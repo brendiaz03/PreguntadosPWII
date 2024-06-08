f2d5e1328fd64546c042a4be2b6176023954a014
Brenda Diaz <brendiaaz03@gmail.com>
2024-06-08 20:27:59 -0300

    b

--- config/config.php
+++ config/config.php
@@ -1,11 +1,11 @@
 <?php
 
     define('SMTP_HOST', 'smtp.gmail.com');
-    define('SMTP_USER', 'brendiaaz03@gmail.com');
-    define('SMTP_PASS', 'kepo brfc jmql jzxg'); // ibpm ylry fram lrcu --- prograweb2
+    define('SMTP_USER', '@gmail.com');
+    define('SMTP_PASS', ''); // ibpm ylry fram lrcu --- prograweb2
     define('SMTP_PORT', 587);
     define('SMTP_SECURE', 'tls');
-    define('FROM_EMAIL', 'brendiaaz03@gmail.com');
+    define('FROM_EMAIL', '@gmail.com');
     define('FROM_NAME', 'Preguntados Admin.');
 
 ?>
