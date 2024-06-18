3cb111917fa9a9031238ddfd61d622400fad5214
FeliWeigel <fweigeldev@gmail.com>
2024-06-18 18:58:59 -0300

    Implementacion editar preguntas

--- controller/AdminController.php
+++ controller/AdminController.php
@@ -29,6 +29,26 @@ class AdminController
         }
     }
 
+    public function editarPregunta(){
+        if($_POST["id"] != null && $_POST["categoria"] != null && $_POST["pregunta"] != null &&
+            $_POST["correcta"] != null && $_POST["opcion2"] != null &&
+            $_POST["opcion3"] != null && $_POST["opcion4"] != null){
+            $idPregunta = $_POST["id"];
+            $categoria = $_POST["categoria"];
+            $pregunta = $_POST["pregunta"];
+            $correcta = $_POST["correcta"];
+            $opcion2 = $_POST["opcion2"];
+            $opcion3 = $_POST["opcion3"];
+            $opcion4 = $_POST["opcion4"];
+
+            $this -> model -> editarPregunta($idPregunta, $categoria, $pregunta, $correcta, $opcion2, $opcion3, $opcion4);
+
+            $this -> presenter -> render("view/editarPreguntaView.mustache", ["success" => "La pregunta se ha modificado correctamente!"]);
+        }else {
+            $this -> presenter -> render("view/editarPreguntaView.mustache", ["error" => "Error! Todos los campos deben completarse"]);
+        }
+    }
+
     public function setEstadoPregunta(){
         $idPregunta = $_POST['idPregunta'];
         $this -> model -> cambiarEstadoPregunta($idPregunta);
@@ -42,9 +62,20 @@ class AdminController
         $this->presenter->render("view/agregarPreguntaView.mustache");
     }
 
+    public function vistaEditarPregunta()
+    {
+        $idPregunta = $_GET['id'];
+        if($idPregunta != null){
+            $datosPregunta = $this -> model -> obtenerPreguntaConRespuestas($idPregunta);
+            $this->presenter->render("view/editarPreguntaView.mustache", $datosPregunta);
+        }
+    }
+
     public function vistaListaPregunta()
     {
         $preguntas = $this->model->getPreguntasEditor();
         $this->presenter->render("view/listaPregunta.mustache", ['preguntas' => $preguntas]);
     }
+
+
 }
--- model/AdminModel.php
+++ model/AdminModel.php
@@ -18,6 +18,25 @@
             $this -> database -> execute($insertRespuestas);
         }
 
+        public function editarPregunta($idPregunta, $categoria, $pregunta, $rtaCorrecta, $opcion2, $opcion3, $opcion4){
+            $updatePregunta = "UPDATE pregunta SET `categoria` = '$categoria', `pregunta` = '$pregunta', `fechaRealizado` = NOW() WHERE `id` = '$idPregunta'";
+            $this -> database -> execute($updatePregunta);
+
+            $selectRespuestas = "SELECT idrespuesta FROM respuesta WHERE pregunta = '$idPregunta'";
+            $respuestas = $this -> database -> query($selectRespuestas);
+            $respuestaIds = array_column($respuestas, 'idrespuesta');
+            $updateRespuestas = [
+                "UPDATE respuesta SET `respuesta` = '$rtaCorrecta', `correcta` = 1 WHERE `idrespuesta` = '{$respuestaIds[0]}'",
+                "UPDATE respuesta SET `respuesta` = '$opcion2', `correcta` = 0 WHERE `idrespuesta` = '{$respuestaIds[1]}'",
+                "UPDATE respuesta SET `respuesta` = '$opcion3', `correcta` = 0 WHERE `idrespuesta` = '{$respuestaIds[2]}'",
+                "UPDATE respuesta SET `respuesta` = '$opcion4', `correcta` = 0 WHERE `idrespuesta` = '{$respuestaIds[3]}'"
+            ];
+
+            foreach ($updateRespuestas as $updateQuery) {
+                $this -> database -> execute($updateQuery);
+            }
+        }
+
         public function cambiarEstadoPregunta($idPregunta){
             $query = "SELECT estado FROM pregunta WHERE id = '$idPregunta' LIMIT 1";
             $result = $this->database->query($query);
@@ -27,6 +46,22 @@
             }else {
                 $this -> database -> execute("UPDATE pregunta SET estado = 'Inactiva' WHERE id = '$idPregunta'");
             }
+        }
+
+        public function obtenerPreguntaConRespuestas($idPregunta) {
+            $sqlPregunta = "SELECT * FROM pregunta WHERE id = '$idPregunta'";
+            $pregunta = $this->database->query($sqlPregunta);
+
+            $sqlRespuestas = "SELECT respuesta FROM respuesta WHERE pregunta = '$idPregunta'";
+            $respuestas = $this->database->query($sqlRespuestas);
+
+            return [
+                'pregunta' => $pregunta[0],
+                'correcta' => $respuestas[0]['respuesta'],
+                'opcion2' => $respuestas[1]['respuesta'],
+                'opcion3' => $respuestas[2]['respuesta'],
+                'opcion4' => $respuestas[3]['respuesta'],
+            ];
         }
 
         public function getPreguntasEditor()
